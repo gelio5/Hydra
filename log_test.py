e9df884dc5fc6366d37b313173b7dc9980e4ca0b
Vladislav <vlreznik97@gmail.com>
2019-11-15 16:37:05 +0300

    some changes

--- log_test.py
+++ log_test.py
@@ -4,6 +4,23 @@ import actuator_lib, pump, system_commands, stand_cooler_lib, thermal_cycler_lib
 import serial.tools.list_ports
 import subprocess
 import time
+import sensors_lib
+#system_commands.PumpToFlowcell(actPos=3, volume=90, aspirationRate=75, dispenseRate=2000)
+#thermal_cycler_lib.SetThermalCyclerTemp(22)
+#thermal_cycler_lib.SetThermalCyclerTempLowSpeed(startTemp=65,stopTemp=40, timeToGo=250)
+#time.sleep(180)
+#for i in range(1):
+#
+#stand_cooler_lib.SetStandDown()
+sensors_lib.BubOn()
+q = time.time()
+time.sleep(5)
+system_commands.PumpToFlowcell(actPos=3, aspirationRate=72, volume=3000, dispenseRate=2000)
+time.sleep(3)
+sensors_lib.BubAsk()
+print(time.time()-q)
+sensors_lib.BubOff()
+
 """
 f = open('./Post_Run_Wash/MiSeqSoftware.00.log', 'r')
 a = f.readlines()
@@ -31,6 +48,9 @@ for i in range(len(a)):
 b.close()
 
 """
+#print(thermal_cycler_lib.GetSpeed())
+#thermal_cycler_lib.SetSpeed(heat=8, cold=-0.5)
+#print(thermal_cycler_lib.GetSpeed())
 # stand_cooler_lib.GetCoolerData()
 # stand_cooler_lib.SetStandDown()
 # stand_cooler_lib.port.close()
@@ -39,7 +59,14 @@ b.close()
 #process_sensors = subprocess.Popen(['python', './Subprocess/ask_sensors.py'])
 #subprocess.check_output(['python', './lib/stand_cooler_lib.py', 'down'])
 #actuator_lib.Test()
-pump.Initialization()
+#pump.Initialization()
+
+#thermal_cycler_lib.SetThermalCyclerTemp(50)
+#time.sleep(120)
+#thermal_cycler_lib.StopThermalCyclerControl()
+#thermal_cycler_lib.SetThermalCyclerTemp(28)
+#time.sleep(20)
+#system_commands.PumpToFlowcell(actPos=3, volume=3000, aspirationRate=50, dispenseRate=2000)
 #subprocess.Popen(['python', './Subprocess/getCool.py'])
 #pump.Test()
 #for i in range(20):
@@ -56,3 +83,12 @@ pump.Initialization()
     time.sleep(60)
     thermal_cycler_lib.SetThermalCyclerTemp(50)
     time.sleep(60)"""
+#stand_cooler_lib.SetStandUp()
+#thermal_cycler_lib.SetThermalCyclerTemp(50)
+#thermal_cycler_lib.TestNewFunction(startTemp=30, stopTemp=65, timeToGo=700)
+#time.sleep(60)
+#thermal_cycler_lib.SetThermalCyclerTempLowSpeed(40)
+#time.sleep(60)
+#system_commands.PumpToActuator(actPos=3)
+#system_commands.PumpToActuator(actPos=3)
+#system_commands.PumpToFlowcell(actPos=2, volume=90, aspirationRate=10, dispenseRate=2000)
--- thermal_cycler_lib.py
+++ thermal_cycler_lib.py
@@ -4,6 +4,7 @@ import serial
 from types_to_sum_of_bytes import *
 from struct import *
 import time
+import redis
 
 port = serial.Serial(port=ports.cycler,
                      baudrate=115200,
@@ -13,6 +14,33 @@ port = serial.Serial(port=ports.cycler,
                      timeout=0.3)
 address = 8
 port.close()
+
+"""
+def SetThermalCyclerTemp(temp: float):
+    try:
+        port.open()
+    except Exception:
+        time.sleep(0.01)
+        port.open()
+    temp = float(temp)
+    length = 8
+    command = 0x91
+    checksum = (address + length + command + FloatToSumOfBytes(temp)) % 256
+    commandToSend = pack("<BBBfB", address, length, command, temp, checksum)
+    port.write(commandToSend)
+    config.logger.info(u'Xmit Cooler %s.' % commandToSend)
+    answerBytes = port.read(4)
+    config.logger.info(u'Recv Cooler: %s' % answerBytes)
+    answer = unpack("<BBBB", answerBytes)
+    if answer[3] != 0x9D:
+        config.logger.error(u'Answer message is corrupted')
+        port.close()
+        SetThermalCyclerTemp(temp)
+    else:
+        port.close()
+        config.logger.info(u'Cooler going to %s' % temp)
+    return
+"""
 
 
 def SetThermalCyclerTemp(temp: float):
@@ -38,6 +66,11 @@ def SetThermalCyclerTemp(temp: float):
     else:
         port.close()
         config.logger.info(u'Cooler going to %s' % temp)
+    conredis = redis.Redis(host='127.0.0.1', port=6379, db=1)
+    ready = 0
+    while ready <= temp - 0.5 or ready >= temp + 0.5:
+        ready = float(conredis.hget("sensors", 'TC').decode())
+        time.sleep(0.5)
     return
 
 
@@ -45,7 +78,7 @@ def StopThermalCyclerControl():
     try:
         port.open()
     except Exception:
-        time.sleep(0.01)
+        time.sleep(0.2)
         port.open()
     length = 4
     command = 0x41
@@ -95,3 +128,95 @@ def GetThermalCyclerData():
     else:
         port.close()
         return answer
+
+
+def GetSpeed():
+    try:
+        port.open()
+    except Exception:
+        time.sleep(0.01)
+        port.open()
+    length = 4
+    command = 0x84
+    checksum = address + length + command
+    commandToSend = pack("<BBBB", address, length, command, checksum)
+    port.write(commandToSend)
+    config.logger.info(u'Xmit Cycler %s.' % commandToSend)
+    answerBytes = port.read(12)
+    config.logger.info(u'Recv Cycler: %s' % answerBytes)
+    answer = unpack("<BBBffB", answerBytes)
+    if answer[5] != (answer[0] +
+                     answer[1] +
+                     answer[2] +
+                     FloatToSumOfBytes(answer[3]) +
+                     FloatToSumOfBytes(answer[4])) % 256:
+        config.logger.error(u'Message is corrupted')
+        port.close()
+        answer = GetSpeed()
+        return answer
+    else:
+        port.close()
+        return answer
+
+
+def SetSpeed(heat, cold):
+    try:
+        port.open()
+    except Exception:
+        time.sleep(0.02)
+        port.open()
+    length = 4
+    command = 0x85
+    checksum = (address + length + command + FloatToSumOfBytes(
+        heat) + FloatToSumOfBytes(cold)) % 256
+    commandToSend = pack("<BBBffB", address, length, command, float(heat),
+                         float(cold), checksum)
+    port.write(commandToSend)
+    config.logger.info(u'Xmit Cycler %s.' % commandToSend)
+    answerBytes = port.read(5)
+    config.logger.info(u'Recv Cycler: %s' % answerBytes)
+    answer = unpack("<BBBBB", answerBytes)
+    if answer[4] != (answer[0] +
+                     answer[1] +
+                     answer[2]) % 256:
+        config.logger.error(u'Message is corrupted')
+        port.close()
+        print('ex')
+        print(answer)
+        return
+    else:
+        port.close()
+        print('god')
+        return
+
+
+def SetThermalCyclerTempLowSpeed(startTemp: float, stopTemp: float, timeToGo: float):
+    try:
+        port.open()
+    except Exception:
+        time.sleep(0.2)
+        port.open()
+    length = 16
+    command = 0x9A
+    checksum = (address + length + command + FloatToSumOfBytes(startTemp) +
+                FloatToSumOfBytes(stopTemp) + FloatToSumOfBytes(
+                timeToGo)) % 256
+    commandToSend = pack("<BBBfffB", address, length, command, startTemp,
+                         stopTemp, timeToGo, checksum)
+    port.write(commandToSend)
+    config.logger.info(u'Xmit Cycler %s.' % commandToSend)
+    answerBytes = port.read(4)
+    config.logger.info(u'Recv Cooler: %s' % answerBytes)
+    answer = unpack("<BBBB", answerBytes)
+    if answer[3] != 0xA6:
+        config.logger.error(u'Answer message is corrupted')
+        port.close()
+        SetThermalCyclerTempLowSpeed(startTemp, stopTemp, timeToGo)
+    else:
+        port.close()
+    conredis = redis.Redis(host='127.0.0.1', port=6379, db=1)
+    ready = 0
+    while ready <= stopTemp - 0.5 or ready >= stopTemp + 0.5:
+        ready = float(conredis.hget("sensors", 'TC').decode())
+        time.sleep(0.5)
+    return
