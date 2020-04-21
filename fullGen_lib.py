c8adeff799cfe65d62dc70f6436c02f4467e1442
Vladislav <vlreznik97@gmail.com>
2020-04-21 17:19:17 +0300

    Changed type of command from integer to string. Arguments for calling in command line was packed in list.

--- fullGen_lib.py
+++ fullGen_lib.py
@@ -6,25 +6,25 @@ from time import sleep
 
 def Snap():
     config.logger.info(u'Start Snap')
-    command = 32860
-    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+    command = "32860"
+    Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
     sleep(3.5)
     config.logger.info(u'End Snap')
 
 
 def Scan():
     config.logger.info(u'Start Scan')
-    command = 32864
-    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+    command = "32864"
+    Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
 
 
 def StartOfNewWorkCycle():
     config.logger.info(u'Restart numeration of scans')
-    command = 32865
-    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+    command = "32865"
+    Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
 
 
 def GoToLoadPos():
     config.logger.info(u'Going cell to load position')
-    command = 32866
-    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+    command = "32866"
+    Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
