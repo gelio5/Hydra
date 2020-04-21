cb37d31ab4d8dae8ed77bc202723b19eb21976a7
gelio5 <vlreznik97@gmail.com>
2020-04-21 14:03:16 +0300

    Update for FullGen 1.0.5.4

--- fullGen_lib.py
+++ fullGen_lib.py
@@ -16,3 +16,15 @@ def Scan():
     config.logger.info(u'Start Scan')
     command = 32864
     Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+
+
+def StartOfNewWorkCycle():
+    config.logger.info(u'Restart numeration of scans')
+    command = 32865
+    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
+
+
+def GoToLoadPos():
+    config.logger.info(u'Going cell to load position')
+    command = 32866
+    Popen("./FullGen_send_command.exe", command, stdout=PIPE)
