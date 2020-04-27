246b5b39013e248fb7f3cf7b39304dd0faaf6a9b
gelio5 <vlreznik97@gmail.com>
2020-04-27 18:43:17 +0300

    Update Scan & Snap commands for using WIN32 API. Add KillFullGen command.

--- fullGen_lib.py
+++ fullGen_lib.py
@@ -2,20 +2,44 @@ from subprocess import Popen
 from subprocess import PIPE
 import config
 from time import sleep
+import redis
+
+connection = redis.Redis(host="127.0.0.1", port=6379, db=1)
 
 
 def Snap():
     config.logger.info(u'Start Snap')
+    connection.set("wParam", '0')
     command = "32860"
     Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
-    sleep(3.5)
+    while 1:
+        wParam = int(connection.get("wParam").decode())
+        if wParam == 3:
+            break
+        elif wParam == 4:
+            lParam = int(connection.get("wParam").decode())
+            print("Was founded trouble in FullGen work. ErrorType = " +
+                  str(lParam))
+        else:
+            sleep(0.1)
     config.logger.info(u'End Snap')
 
 
 def Scan():
     config.logger.info(u'Start Scan')
+    connection.set("wParam", '0')
     command = "32864"
     Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
+    while 1:
+        wParam = int(connection.get("wParam").decode())
+        if wParam == 3:
+            break
+        elif wParam == 4:
+            lParam = int(connection.get("wParam").decode())
+            print("Was founded trouble in FullGen work. ErrorType = " +
+                  str(lParam))
+        else:
+            sleep(0.1)
 
 
 def StartOfNewWorkCycle():
@@ -27,4 +51,10 @@ def StartOfNewWorkCycle():
 def GoToLoadPos():
     config.logger.info(u'Going cell to load position')
     command = "32866"
+    Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
+
+
+def KillFullGen():
+    config.logger.info(u'Quit from FullGen')
+    command = "18"
     Popen(["./FullGen_send_command.exe", command], stdout=PIPE)
